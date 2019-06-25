171457874f99e9888a19397216bf20574af2b9a0
Boris Spasojevic <boris.spasojevic@oracle.com>
2019-06-25 10:52:16 +0200

    Use correct jar in runJsWithCoverage.

--- runJsWithCoverage.sh
+++ runJsWithCoverage.sh
@@ -2,7 +2,7 @@
 
 $JAVA_HOME/bin/js \
     --jvm \
-    --vm.Dtruffle.class.path.append=target/simpletool-1.0-SNAPSHOT.jar \
+    --vm.Dtruffle.class.path.append=target/simpletool-19.0.2-SNAPSHOT.jar \
     --experimental-options \
     --simple-code-coverage.Enable=true \
     src/test/java/com/oracle/simpletool/test/test.js
