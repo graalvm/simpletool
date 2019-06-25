07184fa143d5f9f3caf678ddcebb88a055a80afc
Boris Spasojevic <boris.spasojevic@oracle.com>
2019-06-25 10:17:30 +0200

    Clean up and comment the test file.

--- src/test/java/com/oracle/simpletool/test/SimpleCodeCoverageInstrumentTest.java
+++ src/test/java/com/oracle/simpletool/test/SimpleCodeCoverageInstrumentTest.java
@@ -20,7 +20,8 @@ import org.junit.Test;
 public class SimpleCodeCoverageInstrumentTest {
 
     @Test
-    public void test() throws IOException {
+    public void exampleTest() throws IOException {
+        // This is how we can create a context with our tool enabled if we are embeddined in java
         try (Context context = Context.newBuilder("js")
                 .option(SimpleCodeCoverageInstrument.ID + ".Enable", "true")
                 .option(SimpleCodeCoverageInstrument.ID + ".PrintCoverage", "false")
@@ -28,17 +29,28 @@ public class SimpleCodeCoverageInstrumentTest {
                 .build()) {
             Source source = Source.newBuilder("js", new File("src/test/java/com/oracle/simpletool/test/test.js")).build();
             context.eval(source);
-            SimpleCodeCoverageInstrument coverage = 
-                    context.getEngine().getInstruments().
-                    get(SimpleCodeCoverageInstrument.ID).
-                    lookup(SimpleCodeCoverageInstrument.class);
-            Map<com.oracle.truffle.api.source.Source, Set<SourceSection>> sourceToUncoveredSections = coverage.getSourceToUncoveredSections();
-            Assert.assertEquals(1, sourceToUncoveredSections.size());
-            sourceToUncoveredSections.forEach((com.oracle.truffle.api.source.Source s, Set<SourceSection> v) -> {
-                List<Integer> uncoveredLineNumbers = coverage.uncoveredLineNumbers(s);
-                Object[] expected = new Integer[]{47,48,49,50,51,52,53,54,55,56,57,58,61,67};
-                Assert.assertArrayEquals(expected, uncoveredLineNumbers.toArray());
-            });
+            assertCorrect(context);
         }
+    }
+
+    // NOTE: This lookup mechanism used in this method does not work in normal deployments 
+    // due to Truffles class path issolation.  Services can be looked up by other 
+    // instruments, but not by the embedder. We can do this in the tests because the 
+    // class path issolation is disabled in the pom.xml file by adding -XX:-UseJVMCIClassLoader to the command line. 
+    // This command line flag should never be used in production.
+    private void assertCorrect(final Context context) {
+        // We can lookup services exported by the instrument, in our case this is 
+        // the instrument itself but it does not have to be.
+        SimpleCodeCoverageInstrument coverage = context.getEngine().getInstruments().
+                get(SimpleCodeCoverageInstrument.ID).
+                lookup(SimpleCodeCoverageInstrument.class);
+        // We then use the looked up service to assert that it behaves as expected, just like in any other test.
+        Map<com.oracle.truffle.api.source.Source, Set<SourceSection>> sourceToUncoveredSections = coverage.getSourceToUncoveredSections();
+        Assert.assertEquals(1, sourceToUncoveredSections.size());
+        sourceToUncoveredSections.forEach((com.oracle.truffle.api.source.Source s, Set<SourceSection> v) -> {
+            List<Integer> uncoveredLineNumbers = coverage.uncoveredLineNumbers(s);
+            Object[] expected = new Integer[]{47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 61, 67};
+            Assert.assertArrayEquals(expected, uncoveredLineNumbers.toArray());
+        });
     }
 }
