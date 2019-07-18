2ee9459946b7f8a543fbad405d5f04791d55077c
Boris Spasojevic <boris.spasojevic@oracle.com>
2019-07-18 12:13:58 +0200

    Added simpletools script updated the example.

--- runJsWithCoverage.sh
+++ runJsWithCoverage.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-$JAVA_HOME/bin/js \
-    --jvm \
-    --vm.Dtruffle.class.path.append=target/simpletool-19.0.2-SNAPSHOT.jar \
-    --simple-code-coverage \
-    src/test/java/com/oracle/simpletool/test/test.js
+./simpletool js example.js
